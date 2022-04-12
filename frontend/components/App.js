7a49ecd2b0b90057110ad4bdcb3d6d725488d758
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-12 15:14:21 -0400

    setup state in App component

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const URL = 'http://localhost:9000/api/todos'
+const URL = "http://localhost:9000/api/todos";
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+    completedTodo: false,
+    errorMessage: "",
+    todoName: "",
+  };
+
   render() {
-    return null
+    return null;
   }
 }
